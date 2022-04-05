1eeff2f8a39179576d142745b029242ea996a127
joshi.sumit <joshi.sumit@flipkart.com>
2022-04-05 12:23:46 +0530

    lat long to pincode

---
+++ src/main/java/com/flipkart/logisticsadaptor/utils/CSVUtils.java
@@ -0,0 +1,34 @@
+package com.flipkart.logisticsadaptor.utils;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.ws.rs.core.Response;
+import java.io.*;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Slf4j
+public class CSVUtils {
+
+    public static <T> List<T> fetchInputRecords(InputStream uploadedInputStream, Class<T> type) throws Exception{
+        try {
+            Reader inputReader = new InputStreamReader(uploadedInputStream);
+            CsvToBean<T> csvToBean = new CsvToBeanBuilder<T>(inputReader)
+                    .withType(type)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            return csvToBean.parse();
+        } catch (Exception e) {
+            log.error("Exception while converting the csv record :{} ", e);
+            throw e;
+        }
+    }
+
+
+}
---
+++ src/test/java/com/flipkart/logisticsadaptor/utils/CSVUtilsTest.java
@@ -0,0 +1,13 @@
+package com.flipkart.logisticsadaptor.utils;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class CSVUtilsTest extends TestCase {
+
+    @Test
+    public static void testCsv(){
+
+    }
+
+}
